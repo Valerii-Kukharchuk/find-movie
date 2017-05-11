c717cd70ab326bf4b63617137f980a8f192e832e
Valerii Kukharchuk <valerii.kukharchuk@gmail.com>
2017-05-11 07:21:30 +0300

    first commit for the master-branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,35 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MdToolbarModule} from '@angular/material';
+import {MdCardModule, MdGridListModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilmCardsComponent } from './film-cards/film-cards.component';
+import { FilmCardComponent } from './film-card/film-card.component';
+
+import { FilmService } from './services/film.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilmCardsComponent,
+    FilmCardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdCardModule,
+    MdGridListModule
   ],
-  providers: [],
+  providers: [FilmService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
