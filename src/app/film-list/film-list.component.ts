64303c6ff944ecbb2df610521f04bbc8c0716127
Valerii Kukharchuk <valerii.kukharchuk@gmail.com>
2017-05-28 19:06:52 +0300

    preparation to add new portion of films through the  page property

--- src/app/film-list/film-list.component.ts
+++ src/app/film-list/film-list.component.ts
@@ -17,23 +17,46 @@ export class FilmListComponent implements OnInit {
   private films: Film[] = [];
   private subscription: Subscription;
 
+  private description :string;
+  private searchText :string;
+  private page :number;
+
   constructor(
      private filmsService: FilmService,
      private searchService: SearchTextService) {}
 
-  getFilms(searchText :string) {
+  addFilms(searchText :string) {
+    this.description = "You are searching now...";
     this.films = [];
     this.filmsService.getFilms(searchText).subscribe(
       (film :Film) => this.films.push(film)
-    );
+    );    
   }
 
-  ngOnInit() {    
-    let startSearchText: string = "lord";
-    this.getFilms(startSearchText);
+  newSearchingFilm() {
+    this.description = "You are searching now...";
+    this.films = [];
+    this.filmsService.getFilms(this.searchText).subscribe(
+      (film :Film) => this.films.push(film)
+    );    
+  }
+
+  ngOnInit() {            
+    this.searchText = "lord";    
+    
+    this.newSearchingFilm();
+    this.description = "The most searching movies";
+    this.page = 1;
 
     this.subscription = this.searchService
-      .getSearchText().subscribe((searchText: string) => this.getFilms(searchText));
+      .getSearchText().subscribe((searchText: string) => {
+        this.searchText = searchText;
+        this.newSearchingFilm();
+      });
+  }
+
+  addNextFilmsPage() {
+    this.page++;
   }
 
 }
