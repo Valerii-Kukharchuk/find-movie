e9570c1fe54a3c338f545891e83901ccef73519e
Valerii Kukharchuk <valerii.kukharchuk@gmail.com>
2017-06-08 11:10:01 +0300

    refactoring using a new function of FilmService

--- src/app/film-list/film-list.component.ts
+++ src/app/film-list/film-list.component.ts
@@ -14,13 +14,16 @@ import { SearchTextService } from '../services/search-text.service';
   templateUrl: './film-list.component.html',
   styleUrls: ['./film-list.component.css']
 })
-export class FilmListComponent implements OnInit {
+export class FilmListComponent implements OnInit  {
   private films: Film[] = [];
   private subscription: Subscription;
 
   private description :string;
   private searchText :string;
   private page :number;
+  private totalResults :number;
+
+  private isAddingNewFilms: boolean = false;
 
   views = [
     {value: 'poster-right', viewValue: 'Poster Card View'},
@@ -34,28 +37,38 @@ export class FilmListComponent implements OnInit {
      private searchService: SearchTextService) {}
 
   addFilms() {
-    this.filmsService.getFilms(this.searchText, this.page).subscribe(
-      (film :Film) => this.films.push(film)
+    this.isAddingNewFilms = true;
+    this.filmsService.getSearchFilmResult(this.searchText, this.page).subscribe(
+      (arg :SearchFilmResult) => {
+          arg.films.forEach(film => this.films.push(film));
+          this.totalResults = arg.totalResults;
+      },
+      (error :any) => this.isAddingNewFilms = false,
+      () => this.isAddingNewFilms = false
     );    
   }
 
   newSearchingFilm() {
     this.description = "Searching ...";
     this.films = [];
-    this.filmsService.getFilms(this.searchText).subscribe(
-      (film :Film) => this.films.push(film),
+    this.page = 1;
+    this.filmsService.getSearchFilmResult(this.searchText).subscribe(
+      (arg :SearchFilmResult) => {
+          this.films = arg.films;
+          this.totalResults = arg.totalResults;
+      },
       (error: any) => this.description = "No found result",
       () => {
         if( !this.films.length ) { 
           this.description = "No found result";
         } else {
-          this.description = "Searching result:";
+          this.description = "Searching result:";      
         } }
     );    
   }
 
   ngOnInit() {            
-    this.searchText = "lord";    
+    this.searchText = "lord";
     
     this.newSearchingFilm();
     this.description = "The most searching movies";
